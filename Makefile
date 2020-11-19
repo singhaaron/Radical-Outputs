b7e2b6b79ccb0ef2dbbee2a123c7ae9ee6b9183c
singhaaron <dashboy247@gmail.com>
2020-11-19 11:52:15 -0800

    Thu Nov 19 11:52:15 PST 2020: Tea_Cup

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ LOGFILE=$(LOGPATH)$(shell date)
 
 makeBuild:
 	gcc main.c navigation.c -o execMain -l wiringPi -lpthread
-
+# do gitPush USER="INPUT_NAME"
 gitPush: 
 	git add .
 	git commit -m "${LOGFILE}: ${USER}"
--- navigation.c
+++ navigation.c
@@ -10,36 +10,60 @@
 * Vehicle Functions
 ***************************************************************/
 #include "navigation.h"
+//DMX:Direction Motor Type , VMX:Voltage Motor Type, Direction: 0 or 1, High-Forward,Low-Reverse
+#define START_MOTOR(DMX, VMX, DIRECTION)           \
+    for (int index = 25; index < 125; index += 25) \
+    {                                              \
+        digitalWrite(DMX, DIRECTION);              \
+        softPwmWrite(VMX, index);                  \
+        delay(500);                                \
+    }
+#define STOP_MOTOR(DMX, VMX, DIRECTION)            \
+    for (int index = 100; index != 0; index -= 25) \
+    {                                              \
+        digitalWrite(DMX, DIRECTION);              \
+        softPwmWrite(VMX, index);                  \
+        delay(500);                                \
+    }
 
-#define START_FORWARDMOTOR                         \
-    for (int index = 25; index < 125; index += 25) \
-    {                                              \
-        digitalWrite(DIRECTION_MOTOR_A, 1);        \
-        softPwmWrite(VOLTAGE_MOTOR_A, index);      \
-        delay(500);                                \
-    }
-#define START_REVERSEMOTOR                         \
-    for (int index = 25; index < 125; index += 25) \
-    {                                              \
-        digitalWrite(DIRECTION_MOTOR_C, 1);        \
-        softPwmWrite(VOLTAGE_MOTOR_C, index);      \
-        delay(500);                                \
-    }
+//Just here for Tempalte
+bool obstacle, trail = false;
+
 //Motor A & B - Forward Motion
 void *MotorA()
 {
-    START_FORWARDMOTOR; //Gradually Churn
+    START_MOTOR(DIRECTION_MOTOR_A, VOLTAGE_MOTOR_A, 1); //Gradually Churn Forward
     while (triggerForward)
     {
-        (true /*Check states if reversal & Stop is needed*/) ? printf("ContinueExec") : printf("Break & Call Reversal Motor ");
+        (true /*Check states if reversal & Stop is needed*/) ? printf("ContinueExec") : printf("Break");
+        if (obstacle)
+        {
+            STOP_MOTOR(DIRECTION_MOTOR_A, VOLTAGE_MOTOR_A, 1)
+            triggerForward = false;
+        }
+        if (trail)
+        {
+            STOP_MOTOR(DIRECTION_MOTOR_A, VOLTAGE_MOTOR_A, 1)
+            triggerForward = false;
+        }
     }
 }
 //Motor C & D - Reverse Motion
 void *MotorC()
 {
-    START_REVERSEMOTOR;
+    START_REVERSEMOTOR(DIRECTION_MOTOR_C, VOLTAGE_MOTOR_C, 0); //Gradually Churn Reverse
     while (triggerReverse)
     {
-        (true /*Check states if forward or stop is needed*/) ? printf("ContinueExec") : printf("Break & Call Forward Motor ");
+        (true /*Check State/Condition */) ? printf("ContinueExec") : printf("Break");
+        if (obstacle)
+        {
+            STOP_MOTOR(DIRECTION_MOTOR_C, VOLTAGE_MOTOR_C, 0)
+            triggerReverse = false;
+        }
+        if (trail)
+        {
+            STOP_MOTOR(DIRECTION_MOTOR_C, VOLTAGE_MOTOR_C, 0)
+            triggerReverse = false;
+        }
     }
 }
